5aae9f7c401c932df17e1adf60dad16756c589a2
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-25 18:34:35 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/prospec2mongo/Prospec2mongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.prospec2mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prospec2mongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prospec2mongoApplication.class, args);
+	}
+
+}
